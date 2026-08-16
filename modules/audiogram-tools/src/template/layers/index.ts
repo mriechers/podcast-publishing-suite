2f9f2f65bc9cf594c78a14db0903b0ce4e972728
Mark Riechers <markriechers@gmail.com>
2026-08-16 00:11:49 -0500

    chore(audiogram-tools): remove flattened snapshot ahead of subtree import

--- modules/audiogram-tools/src/template/layers/index.ts
+++
@@ -1,4 +0,0 @@
-export { SolidBackground } from "./SolidBackground";
-export { GalaxySpiral } from "./GalaxySpiral";
-export { CabinetFrame } from "./CabinetFrame";
-export { TitleImage } from "./TitleImage";
